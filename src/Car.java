4fb221d6d27c1adec12cc7ef4e118dae88285f83
Martin Baaring <martinbaaring@gmail.com>
2011-11-22 10:25:05 +0100

    Signed-off-by: Martin Baaring <martinbaaring@gmail.com>

---
+++ src/Car.java
@@ -0,0 +1,19 @@
+/**
+ * @author Anders
+ *
+ */
+public class Car {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		public void print()
+		{
+			System.out.println("Hello world")
+		}
+	}
+
+}
---
+++ src/Customer.java
@@ -0,0 +1,15 @@
+/**
+ * @author Anders
+ *
+ */
+public class Customer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
